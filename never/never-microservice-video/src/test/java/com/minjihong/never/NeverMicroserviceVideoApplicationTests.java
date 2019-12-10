2e4f0c42e2d0bda9c9c2865199ae545982e6c9b2
闵继弘 <15995283349@163.com>
2019-12-10 18:04:11 +0800

    video module create

---
+++ never/never-microservice-video/src/test/java/com/minjihong/never/NeverMicroserviceVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minjihong.never;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeverMicroserviceVideoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
