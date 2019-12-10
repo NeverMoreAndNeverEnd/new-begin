97074247669a6ba62a574ae9d9ab352918eaaeaf
闵继弘 <15995283349@163.com>
2019-12-10 17:22:24 +0800

    test Aliyun video play

--- never/never-framework/src/main/java/com/minjihong/never/admin/oss/AliyunOss.java
+++ never/never-framework/src/main/java/com/minjihong/never/admin/oss/AliyunOss.java
@@ -16,9 +16,9 @@ public class AliyunOss {
         // Endpoint以杭州为例，其它Region请按实际情况填写。
         String endpoint = "http://oss-cn-shanghai.aliyuncs.com";
         // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-        String accessKeyId = "LTAIzm0wduyxbxxq";
-        String accessKeySecret = "tYiGJDT7feGekmVwa2nTTqLya1I7tN";
-        String bucketName = "never-edu-test";
+        String accessKeyId = "";
+        String accessKeySecret = "";
+        String bucketName = "";
 
         // 创建OSSClient实例。
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
@@ -34,15 +34,15 @@ public class AliyunOss {
         // Endpoint以杭州为例，其它Region请按实际情况填写。
         String endpoint = "http://oss-cn-shanghai.aliyuncs.com";
         // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-        String accessKeyId = "LTAIzm0wduyxbxxq";
-        String accessKeySecret = "tYiGJDT7feGekmVwa2nTTqLya1I7tN";
+        String accessKeyId = "";
+        String accessKeySecret = "";
 
         // 创建OSSClient实例。
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
 
         // 上传文件流。
         InputStream inputStream = new FileInputStream("<yourlocalFile>");
-        ossClient.putObject("never-edu-test", "<yourObjectName>", inputStream);
+        ossClient.putObject("", "<yourObjectName>", inputStream);
 
         // 关闭OSSClient。
         ossClient.shutdown();
---
+++ never/never-framework/src/main/java/com/minjihong/never/admin/video/TestVideo.java
@@ -0,0 +1,36 @@
+package com.minjihong.never.admin.video;
+
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.vod.model.v20170321.GetPlayInfoRequest;
+import com.aliyuncs.vod.model.v20170321.GetPlayInfoResponse;
+
+import java.util.List;
+
+public class TestVideo {
+
+    /*获取播放地址函数*/
+    public static GetPlayInfoResponse getPlayInfo(DefaultAcsClient client) throws Exception {
+        GetPlayInfoRequest request = new GetPlayInfoRequest();
+        request.setVideoId("");
+        return client.getAcsResponse(request);
+    }
+    /*以下为调用示例*/
+    public static void main(String[] argv) throws ClientException {
+        DefaultAcsClient client = AliyunVideoSDKUtils.initVodClient("", "");
+        GetPlayInfoResponse response = new GetPlayInfoResponse();
+        try {
+            response = getPlayInfo(client);
+            List<GetPlayInfoResponse.PlayInfo> playInfoList = response.getPlayInfoList();
+            //播放地址
+            for (GetPlayInfoResponse.PlayInfo playInfo : playInfoList) {
+                System.out.print("PlayInfo.PlayURL = " + playInfo.getPlayURL() + "\n");
+            }
+            //Base信息
+            System.out.print("VideoBase.Title = " + response.getVideoBase().getTitle() + "\n");
+        } catch (Exception e) {
+            System.out.print("ErrorMessage = " + e.getLocalizedMessage());
+        }
+        System.out.print("RequestId = " + response.getRequestId() + "\n");
+    }
+}
